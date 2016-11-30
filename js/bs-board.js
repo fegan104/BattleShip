f755be50df6515460bc0de041915bcb756100a1d
fegan104 <fegan104@gmail.com>
2016-11-30 12:09:57 +0100

    fixed really dumb issue where jsboard.min.js was not being loaded OR it was preventing the index.htl header from being parsed

--- js/bs-board.js
+++ js/bs-board.js
@@ -1,9 +1,9 @@
 // create board
 var b = jsboard.board({attach:"battle-ship-board", size:"10x10"});
 b.cell("each").style({
-	height:"32px", 
-	width:"32px",
-	background: "#4990E2"
+	background: "#4990E2",
+	textAlign:"center",
+	overflow:"visible"
 });
 
 // Battleship Pieces
@@ -15,12 +15,29 @@ var aim = jsboard.piece({
 });
 var miss = jsboard.piece({text:"miss", fontSize:"45px", textAlign:"center"});
 var hit = jsboard.piece({text:"hit", fontSize:"45px", textAlign:"center"});
-var boat = jsboard.piece({text:"boat", fontSize:"45px", textAlign:"center"});
+var boat = jsboard.piece({
+	text:"aim",
+	width: "5vw",
+    height: "5vw",
+	textIndent:"-9999px",
+	background:"#000080",
+	margin:"0 auto"
+});
 
-// alternate turns of x and o
-var turn = true;
+//Clicking on a tile set the cursor for aim
 b.cell("each").on("click", function() {
     if (b.cell(this).get()==null) {
         b.cell(this).place(aim.clone());
+    }
+});
+//By default elements can't drag and drop
+b.cell("each").on("dragover", function allowDrop(ev) {ev.preventDefault();});
+// Handle drop event for placing boats
+b.cell("each").on("drop", function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("boat");
+    if (data == "aircraft-carrier") {
+    	b.cell(this).place(boat.clone());
+    	document.getElementById(data).style.display = 'none';
     }
 });
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-console.info('Service worker disabled for development, will be generated at build time.');
+console.log("service worker");
